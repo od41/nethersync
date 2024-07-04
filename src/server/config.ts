e25d857ba81c6fcd0adf68c87b46a6fb8171707e
Odafe Aror <odafe41@gmail.com>
2024-07-04 07:12:09 +0000

    remove @upstash/redis

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,10 +1,3 @@
-import { Redis } from "@upstash/redis";
-
-export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL! || "",
-  token: process.env.UPSTASH_REDIS_TOKEN! || "",
-});
-
 export const APILLION_KEY_SECRET = process.env.APILLION_KEY_SECRET!;
 export const APILLION_API_KEY = process.env.APILLION_API_KEY!;
 
